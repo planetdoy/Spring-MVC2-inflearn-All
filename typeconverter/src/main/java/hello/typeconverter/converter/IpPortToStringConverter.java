c94bd7b7f3a07139ee7ca19f686c6f88c869e0d5
planetdoy <doydoit@gmail.com>
2021-08-13 15:40:18 +0900

    feat : Converter 직접 작성 및 테스트
    스프링 MVC 2편 - 스프링 타입 컨버터 : Converter
    Converter 를 직접 작성하여 테스트했습니다.

---
+++ typeconverter/src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
@@ -0,0 +1,12 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.springframework.core.convert.converter.Converter;
+
+public class IpPortToStringConverter implements Converter<IpPort,String> {
+    @Override
+    public String convert(IpPort source) {
+        //IpPort객체 -> "127.0.0.1:8080"
+        return source.getIp() + ":" + source.getPort();
+    }
+}
